6529d16289adb0d4fed0824b65a53fbfa29e3d97
Lilli Thomas <lilli.thomas@hsw-stud.de>
2023-11-02 11:39:16 +0100

    Aufgabe 6

---
+++ app/src/test/java/hsw/gradle/template/IBANTest.java
@@ -0,0 +1,30 @@
+package hsw.gradle.template;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IBANTest {
+
+    @Test void IBANDECheck(){
+        String myIBAN = "DE68210501700012345678";
+        IBAN classUnderTest = new IBAN(myIBAN);
+        assertEquals(classUnderTest.checkDE(myIBAN), true);
+    }
+
+    @Test void IBANDEWrong(){
+        String myIBAN = "ZZ68210501700012345678";
+        IBAN classUnderTest = new IBAN(myIBAN);
+        assertEquals(classUnderTest.check(myIBAN), false);
+    }
+    @Test void IBANDE() {
+        String myIBAN = "DE68210501700012345678";
+        IBAN classUnderTest = new IBAN(myIBAN);
+        assertEquals(classUnderTest.check(myIBAN), true);
+    }
+
+    @Test void IBANWrong() {
+        String myIBAN = "ZZ68210501700012345678";
+        IBAN classUnderTest = new IBAN(myIBAN);
+        assertEquals(classUnderTest.check(myIBAN), false);
+    }
+}
